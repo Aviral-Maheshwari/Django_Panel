e4959c75760a9f9457727ddcbc5ed6ee36d99f6b
AVIRAL MAHESHWARI <techaviral@gmail.com>
2025-11-03 21:34:41 +0530

    Create Serializers

---
+++ backend-portal/accounts/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    
+    def create(self,validated_data):
+        # User.objects.create = save the password in a plain text 
+        #User.objects.create_user = save the password in hash automatically
+        
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        # user = User.objects.create_user(**validated_data)
+        return user
+
+
--- react_portal/src/components/Main.jsx
+++ react_portal/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
                     <h1 className='text-light'> Stock Prediction Portal</h1>
                     <p className='text-light lead'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium nobis id repellendus quaerat culpa omnis sunt libero recusandae. Labore quos aliquid, molestiae optio nam libero repellat iusto nulla eveniet culpa.</p>
 
-                    <Button text="Login" class="btn-outline-danger" />
+                    <Button text="Login" class="btn-outline-info" />
 
                 </div>
             </div>
